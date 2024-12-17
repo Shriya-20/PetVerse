ebebce25eaa1ba118db58039a3208041b505e2b0
Sathvik <sathvikmu04@gmail.com>
2024-12-17 12:08:56 +0530

    no

--- petverse/app/components/EditProfile.js
+++ petverse/app/components/EditProfile.js
@@ -42,8 +42,8 @@ export default function EditProfile() {
   };
 
   const handleChangeLocation = () => {
-    return
-  }
+    return;
+  };
 
   const data = {
     username: "Sathvik",
@@ -53,67 +53,54 @@ export default function EditProfile() {
   };
 
   return (
-    <div className="grid grid-cols-3 gap-4 p-4">
+    <div className="md:grid md:grid-cols-3 gap-4 p-8">
       {/* Profile Picture Section */}
-      <div className="flex flex-col items-center col-span-1">
+      <div className="md:flex md:flex-col items-center col-span-1 block mb-4 pl-8 md:pl-2">
         <ProfileIcon
           profile_pic={LoginDoggy}
-          width="w-[120px]"
-          height="h-[120px]"
+          width="w-[170px]"
+          height="h-[170px]"
           className="border-2 border-white mb-4"
         />
       </div>
 
       {/* User Details and Password Change Section */}
-      <div className="col-span-2 space-y-4">
+      <div className="col-span-2 space-y-4 edit-profile">
         {/* Change Username Section */}
+        <input placeholder="Enter new name" className="" />
+        <button className="">Change Name</button>
         <input
-            placeholder="Enter new name"
-            className="flex-grow p-2 border border-gray-300 rounded-md w-2/3"
-          />
-          <button className="custom-button w-1/3  px-4 py-2 rounded-md">
-            Change Name
-          </button>
-          <input
-            name="password"
-            placeholder="Enter old password"
-            onChange={handleChangePasswordData}
-            className="flex-grow p-2 border border-gray-300 rounded-md block w-2/3"
-          />
-          <input
-            name="newPassword"
-            placeholder="Enter new password"
-            onChange={handleChangePasswordData}
-            className="flex-grow p-2 border border-gray-300 rounded-md w-2/3 block"
-          />
-            <input
-              name="confirmPassword"
-              placeholder="Confirm new password"
-              onChange={handleChangePasswordData}
-              className="flex-grow p-2 border border-gray-300 rounded-md w-2/3"
-            />
-            <button
-              onClick={handleChangePassword}
-              className="custom-button w-1/3  px-6 py-2 rounded-md shrink-0"
-            >
-              Change Password
-            </button>
-            <input
-              name="location"
-              placeholder="Enter Location"
-              onChange={handleChangeLocation}
-              className="flex-grow p-2 border border-gray-300 rounded-md w-2/3"
-            />
-            <button
-              onClick={handleChangeLocation}
-              className="custom-button px-6 py-2 rounded-md w-1/3"
-            >
-              Change Location
-            </button>
+          name="password"
+          placeholder="Enter old password"
+          onChange={handleChangePasswordData}
+          className=""
+        />
+        <input
+          name="newPassword"
+          placeholder="Enter new password"
+          onChange={handleChangePasswordData}
+          className=""
+        />
+        <input
+          name="confirmPassword"
+          placeholder="Confirm new password"
+          onChange={handleChangePasswordData}
+          className=""
+        />
+        <button onClick={handleChangePassword} className="">
+          Change Password
+        </button>
+        <input
+          name="location"
+          placeholder="Enter Location"
+          onChange={handleChangeLocation}
+          className="3"
+        />
+        <button onClick={handleChangeLocation} className="">
+          Change Location
+        </button>
 
-
-        {/* Change User location Section */ }
-
+        {/* Change User location Section */}
       </div>
     </div>
   );
