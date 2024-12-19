05f679e95845230ebfabb1cb3e29ecd9d60f3b70
sathvik-ujwal <sathvikmu04@gmail.com>
2024-12-20 00:13:33 +0530

    added option to toggle between dark and light mode

--- petverse/app/petverse/layout.js
+++ petverse/app/petverse/layout.js
@@ -10,23 +10,18 @@ export default function Dashboard({ children }) {
   const isMessagesPage = pathname === "/petverse/messages";
 
   return (
-    <>
-      <div className="fixed inset-0 flex h-screen bg-light1 dark:bg-dark1">
-        <div className="lg:w-[245px] w-[55px] sticky top-0">
-          <Sidebar />
-        </div>
-        <div
-          className={clsx(
-            "flex-grow overflow-auto max-h-screen p-0 mr-2 pt-2",
-            {
-              "flex-grow overflow-hidden max-h-screen p-0 mr-2 pt-2":
-                isMessagesPage,
-            }
-          )}
-        >
-          {children}
-        </div>
+    <div className="fixed inset-0 flex h-screen bg-light1 dark:bg-dark1">
+      <div className="lg:w-[245px] w-[55px] sticky top-0">
+        <Sidebar />
       </div>
-    </>
+      <div
+        className={clsx("flex-grow overflow-auto max-h-screen p-0 mr-2 pt-2", {
+          "flex-grow overflow-hidden max-h-screen p-0 mr-2 pt-2":
+            isMessagesPage,
+        })}
+      >
+        {children}
+      </div>
+    </div>
   );
 }
