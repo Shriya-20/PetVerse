0e8225a7ebeb3596cc6e33f5c61cc69b6f2725a2
R1THV1K <rithvikmu2004@gmail.com>
2024-12-19 13:55:14 +0530

    made auth pages layout static

--- petverse/app/auth/layout.js
+++ petverse/app/auth/layout.js
@@ -5,8 +5,8 @@ import Image from "next/image";
 export default function AuthenticationLayout({ children }) {
   return (
     <div className="flex max-w-lg mx-auto my-16 overflow-hidden bg-white rounded-lg lg:space-x-8 dark:bg-gray-900 lg:max-w-5xl shadow-2xl">
-      <div className="items-center hidden lg:flex lg:w-1/2 relative">
-        <Image src={loginDoggy} alt="Login image" layout="fill" />
+      <div className=" items-center hidden lg:flex lg:w-1/2 relative">
+        <Image src={loginDoggy} alt="Login image" height={700} width={700} />
       </div>{" "}
       <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">{children}</div>
     </div>
