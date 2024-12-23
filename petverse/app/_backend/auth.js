c5bd1408731094ac43a04b9fb99f198fea5fa4b0
R1THV1K <rithvikmu2004@gmail.com>
2024-12-23 15:46:14 +0530

    added auth with email and password

--- petverse/app/_backend/auth.js
+++ petverse/app/_backend/auth.js
@@ -3,7 +3,7 @@ import {
   signInWithEmailAndPassword,
   signOut,
 } from "firebase/auth";
-import { auth } from "@/firebaseConfig";
+import { auth } from "@/app/_backend/firebaseConfig";
 
 export async function handleSignUpWithEmail(email, password) {
   try {
