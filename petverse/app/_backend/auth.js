d63355110b599fb9ff739c06921f84d84df52f2f
R1THV1K <rithvikmu2004@gmail.com>
2024-12-23 15:45:23 +0530

    configured firebase

---
+++ petverse/app/_backend/auth.js
@@ -0,0 +1,44 @@
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+import { auth } from "@/firebaseConfig";
+
+export async function handleSignUpWithEmail(email, password) {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    console.log("User created");
+    console.log(`User: ${user}`);
+  } catch (error) {
+    console.log(`Error Code: ${error.code}  Error message: ${error.message}`);
+  }
+}
+
+export async function handleSignInWithEmail(email, password) {
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    console.log("User created");
+    console.log(`User: ${user}`);
+  } catch (error) {
+    console.log(`Error Code: ${error.code}  Error message: ${error.message}`);
+  }
+}
+
+export async function handleSignOut() {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    console.log(`Error when logging out: ${error}`);
+  }
+}
