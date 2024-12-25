5be575962bc38864a0a9108192888a0e1b35275b
sathvik-ujwal <sathvikmu04@gmail.com>
2024-12-25 21:50:37 +0530

    no changes

--- petverse/app/api/pets/userpets/route.js
+++ petverse/app/api/pets/userpets/route.js
@@ -1,17 +1,17 @@
 import { connectToDatabase } from "@/app/utils/db";
+import { ObjectId } from "mongodb";
 
 export async function POST(req) {
   try {
-    const body = await req.json();
-    const { userId } = body;
+    const userId = await req.json();
     const db = await connectToDatabase();
     const userPets = await db
-      .collection("pets")
-      .find({ owner: userId })
+      .collection("pet")
+      .find({ owner: new ObjectId(userId) })
       .toArray();
+
     return new Response(JSON.stringify(userPets), { status: 200 });
   } catch (error) {
-    console.error("Error getting pet data", error);
     return new Response(
       JSON.stringify({ error: "Error getting user pet data" }),
       { status: 500 }
