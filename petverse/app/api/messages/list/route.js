383bbc53c84273672776040af0b9f58cd5402803
sathvik-ujwal <sathvikmu04@gmail.com>
2024-12-31 02:39:52 +0530

    messenging

--- petverse/app/api/messages/list/route.js
+++ petverse/app/api/messages/list/route.js
@@ -5,20 +5,23 @@ import { ref, child, get } from "firebase/database";
 export async function POST(req) {
   try {
     const { userId } = await req.json();
+    console.log("fetched id");
     const dbRef = ref(database);
-    get(child(dbRef, `chats/${userId}`))
-      .then((snapshot) => {
-        if (snapshot.exists()) {
-          console.log(snapshot.val());
-        } else {
-          console.log("No data available");
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-    return NextResponse.json();
+    console.log("Fetched database ref");
+    console.log(userId);
+    console.log("NO ERROR");
+    const snapshot = await get(child(dbRef, `chats/${userId}`));
+
+    if (snapshot.exists()) {
+      const data = snapshot.val();
+      console.log(data);
+      return NextResponse.json(data, { status: 200 });
+    } else {
+      console.log("No data available");
+      return NextResponse.json([], { status: 200 });
+    }
   } catch (error) {
+    console.log(error);
     return NextResponse.json(
       { error: "Error in fetching chats" },
       { status: 401 }
