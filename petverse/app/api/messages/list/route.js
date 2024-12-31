b44fedc4240df7004752b8f5539639e86da73d1d
sathvik-ujwal <sathvikmu04@gmail.com>
2025-01-01 00:23:35 +0530

    just removed log statements

--- petverse/app/api/messages/list/route.js
+++ petverse/app/api/messages/list/route.js
@@ -5,16 +5,11 @@ import { ref, child, get } from "firebase/database";
 export async function POST(req) {
   try {
     const { userId } = await req.json();
-    console.log("fetched id");
     const dbRef = ref(database);
-    console.log("Fetched database ref");
-    console.log(userId);
-    console.log("NO ERROR");
     const snapshot = await get(child(dbRef, `chats/${userId}`));
 
     if (snapshot.exists()) {
       const data = snapshot.val();
-      console.log(data);
       return NextResponse.json(data, { status: 200 });
     } else {
       console.log("No data available");
