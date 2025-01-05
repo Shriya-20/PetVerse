4ceae218b97d57f34c9a317399cb009af4e9b317
sathvik-ujwal <sathvikmu04@gmail.com>
2025-01-05 17:31:13 +0530

    added deleting chats

--- petverse/app/api/users/delete/route.js
+++ petverse/app/api/users/delete/route.js
@@ -3,6 +3,9 @@ import { NextResponse } from "next/server";
 import { ObjectId } from "mongodb";
 import { handleDeleteUser } from "@/app/_backend/auth";
 import { deleteFolder } from "@/app/actions";
+import { database } from "@/app/_backend/firebaseConfig";
+import { ref, remove, get, update } from "firebase/database";
+import { ContactsOutlined } from "@mui/icons-material";
 
 export async function POST(req) {
   try {
@@ -10,8 +13,10 @@ export async function POST(req) {
     const db = await connectToDatabase();
 
     // Delete pet data from firebase storage
-    const userData = await db.collection("users").findOne({_id: new ObjectId(userId)});
-    if(userData.pets){
+    const userData = await db
+      .collection("users")
+      .findOne({ _id: new ObjectId(userId) });
+    if (userData.pets) {
       for (const pet of userData.pets) {
         const petId = pet.toString();
         await deleteFolder(`${petId}`);
@@ -19,16 +24,19 @@ export async function POST(req) {
     }
 
     // Delete user items data from firebase storage
-    const userItems = await db.collection("marketplaceitems").find({sellerId: new ObjectId(userId)}, { _id: 1}).toArray();
-    if(userItems.len != 0){
-      for (const userItem of userItems){
+    const userItems = await db
+      .collection("marketplaceitems")
+      .find({ sellerId: new ObjectId(userId) }, { _id: 1 })
+      .toArray();
+    if (userItems.len != 0) {
+      for (const userItem of userItems) {
         const itemId = userItem._id;
-        await deleteFolder(`item/${itemId}`)
+        await deleteFolder(`item/${itemId}`);
       }
     }
-    
+
     // Delete user profile data from firebase storage
-    await deleteFolder(`users/${userId}`)
+    await deleteFolder(`users/${userId}`);
 
     // Delete user, user pets and user items from db
     await db.collection("users").deleteOne({ _id: new ObjectId(userId) });
@@ -37,11 +45,43 @@ export async function POST(req) {
       .collection("marketplaceitems")
       .deleteMany({ sellerId: new ObjectId(userId) });
 
+    //Delete user chats from firebase realtime database and update chatlist
+    const defaultProfilePic =
+      "https://firebasestorage.googleapis.com/v0/b/petverse-3fa63.firebasestorage.app/o/default_user_profile_pic.jpeg?alt=media&token=8fc7a77a-e2d1-499c-b986-ade30125b8bc";
+
+    const userChatRef = ref(database, `chats/${userId}`);
+
+    try {
+      const snapshot = await get(userChatRef);
+      console.log(snapshot.val());
+      if (!snapshot.exists()) {
+        console.log("No chats to delete");
+        return;
+      }
+
+      const otherUsers = Object.keys(snapshot.val());
+      await remove(userChatRef);
+      console.log("Successfully deleted user chats");
+
+      const updates = {};
+      otherUsers.forEach((otherUserId) => {
+        updates[`chats/${otherUserId}/${userId}/name`] = "Deleted User";
+        updates[`chats/${otherUserId}/${userId}/profile_picture`] =
+          defaultProfilePic;
+      });
+
+      await update(ref(database), updates);
+      console.log("Successfully updated other users chat data");
+    } catch (error) {
+      console.log("Failed to deleted stuff");
+      console.log(error);
+    }
+
     // Delete user from firebase auth
     await handleDeleteUser();
 
     return NextResponse.json("Successfully deleted user", { status: 200 });
   } catch (error) {
-    return NextResponse.json({error: error.code}, { status: 401 });
+    return NextResponse.json({ error: error.code }, { status: 401 });
   }
 }
