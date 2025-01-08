acfe140f589859d4e40febe43eac744c54def782
sathvik-ujwal <sathvikmu04@gmail.com>
2025-01-08 19:20:09 +0530

    added option to scale the gif

--- petverse/app/components/Loading2.js
+++ petverse/app/components/Loading2.js
@@ -1,12 +1,18 @@
 import loadinggif from "@/public/loading2.gif";
 import Image from "next/image";
 
-export default function Loading({ isLoading }) {
+export default function Loading({ isLoading, scale = 100 }) {
   if (!isLoading) return null;
 
   return (
-    <div className="absolute inset-0 flex items-center justify-center z-10">
-      <Image src={loadinggif} alt="Loading..." height={20} width={20} />
+    <div className="absolute inset-0 flex items-center justify-center z-50">
+      <Image
+        src={loadinggif}
+        alt="Loading..."
+        height={20}
+        width={20}
+        style={{ transform: `scale(${scale / 100})` }}
+      />
     </div>
   );
 }
