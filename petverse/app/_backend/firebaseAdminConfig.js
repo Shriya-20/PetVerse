1a758e319fca609157eb6c44cd9cd009f7b7b80a
Rithvik <rithvikmu2004@gmail.com>
2025-01-10 18:10:52 +0530

    added firebase admin to server

---
+++ petverse/app/_backend/firebaseAdminConfig.js
@@ -0,0 +1,10 @@
+import admin from "firebase-admin";
+import serviceAccount from "../../config/serviceAccountKey.json";
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL:
+    "https://petverse-3fa63-default-rtdb.asia-southeast1.firebasedatabase.app",
+});
+
+export { admin };
